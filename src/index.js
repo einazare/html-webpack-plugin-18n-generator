e8d714249a3c5d3978ef0982564385c66ac0cc40
Nazare Emanuel-Ioan <emanuelioannazare@gmail.com>
2023-10-04 16:15:14 +0300

    Revert "Using the new html-bundler-webpack-plugin to fix all issues"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 (function () {
-  // note: the `locales` is the stringified JSON from <script>var locales = {title:'...', ...};</script> tag in HTML
-  console.log("title: ", locales.title, { locales });
+  console.log("<%= title %>");
 })();
